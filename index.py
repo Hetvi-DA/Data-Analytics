9d4c415254fa352203872f132e882c4c7de97ced
Hetvi-DA <joshihetvii29@gmail.com>
2026-06-27 21:05:23 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+# print("Namaste Bharat")
+# print("Hello Rohan")
+
+# print("Hetvi")
+# print("Ahmedabad")
+# print("Welcome to data analytics")
+
+# Name = input("What is your name: ")
+# Age = int(input("What is your age: "))
+
+# print(Name)
+# print(Age)
+
+# print("My name is", Name,"and i am" ,Age," years old")
+
+Name = input("What is your name: ")
+Course = input("What is your course: ")
+Fees = int(input("What is your fees: "))
+

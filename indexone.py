93c264a568a30d0f1ab2dd4e2dc87908c3621038
Hetvi-DA <joshihetvii29@gmail.com>
2026-07-18 21:08:55 +0530

    Add files via upload

---
+++ indexone.py
@@ -0,0 +1,71 @@
+# import numpy as np
+
+# arr = np.array([10,20,30])
+
+# print(arr + arr)
+
+# import numpy as np
+
+# salary = np.array([20000, 30000, 40000])
+
+# print(salary * 1.10)
+
+#  Creating Arrays in NumPy
+
+#  Creating 1D Array
+
+# import numpy as np
+
+# marks = np.array([85, 90, 78, 92])
+
+# print(marks)
+
+# 5.2 Creating 2D Array
+
+# Rows and columns.
+# import numpy as np
+# employee_data = np.array([
+#     [101, 25000],
+#     [102, 30000],
+#     [103, 35000]
+# ])
+
+# print(employee_data)
+
+# 5.3 Creating Array using zeros()
+
+# np.zeros(5)
+
+# 2D Example:
+
+# np.zeros((3,4))
+
+# 5.4 Creating Array using ones()
+
+# print(np.ones(5))
+
+# 2D Example:
+
+# print(np.ones((2,3)))
+
+# 5.5 Using arange()
+# Like Python range().
+
+# print(np.arange(1,11))
+
+# Step Example:
+
+# print(np.arange(0,20,2))
+
+# 5.6 Using linspace()
+# Creates evenly spaced values
+
+# print(np.linspace(1,10,5))
+
+# 5.7 Random Number Generation
+
+# Random Integers
+# print(np.random.randint(1,100,10))
+
+# Random Decimal Values
+# print(np.random.random(5))

378ffa4f825506d0d61ac314b242eeaee06380b8
Hetvi-DA <joshihetvii29@gmail.com>
2026-08-09 09:17:14 +0530

    Add files via upload

---
+++ pandastwo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+sales = pd.read_csv("sales_data_large.csv")
+
+# 2.1 isnull()
+# print(sales.isnull())
+
+# Counting Missing Values
+# print(sales.isnull().sum())
+
+# 2.2 notnull()
+# print(sales.notnull())
+

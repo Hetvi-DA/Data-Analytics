7bec98644f602e01f3027637c53576377dd8773d
Hetvi-DA <joshihetvii29@gmail.com>
2026-08-16 12:06:43 +0530

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,131 @@
+import matplotlib.pyplot as plt
+# plt.plot([1,2,3], [10,20,30])
+# plt.show()
+
+# import matplotlib.pyplot as plt
+
+# x = [1,2,3,4,5]
+# y = [10,20,30,40,50]
+
+# plt.plot(x, y)
+# plt.show()
+
+# x= [1,2,3,4,5]
+# y= [10,20,30,40,50]
+# plt.plot(x,y)
+# plt.show()
+
+# months = [1,2,3,4,5]
+# sales= [10000,20000,30000,40000,50000]
+# plt.plot(months,sales)
+# plt.show()
+
+# subjects = [1,2,3,4,5]
+# marks = [55,60,72,85,90]
+
+# plt.plot(subjects, marks)
+# plt.title("Students Marks")
+# plt.xlabel("subjects")
+# plt.ylabel("Marks")
+# plt.grid()
+# plt.show()
+
+# Multiple Line Plot
+# months=[1,2,3,4,5]
+# sales_2024=[100,200,300,400,500]
+# sales_2025=[150,250,350,450,550]
+# plt.plot(months, sales_2024)
+# plt.plot(months,sales_2025)
+# # plt.grid()
+# # plt.title("Sales Trend")
+# # plt.xlabel("Months")
+# # plt.ylabel("Sales-2024-2025")
+# plt.legend()
+# plt.show()
+
+
+
+# months = [1,2,3,4]
+
+# sales_2024 = [100,200,300,400]
+# sales_2025 = [150,250,350,500]
+
+# plt.plot(months, sales_2024, label="2024 year")
+# plt.plot(months, sales_2025, label="2025 year")
+
+# plt.legend()
+
+# plt.show()
+
+
+# 16. Real Business Example
+
+# months = ["Jan","Feb","Mar","Apr"]
+
+# amazon_sales = [20000,30000,35000,45000]
+
+# plt.plot(months, amazon_sales)
+
+# plt.title("Amazon Monthly Sales")
+# plt.xlabel("Month")
+# plt.ylabel("Revenue")
+# plt.grid()
+
+# plt.show()
+
+# ------------------ Practice Questions
+
+# Question 1
+# Create line chart for:
+# students=[1,2,3,4,5]
+# marks = [50,60,70,80,90]
+# plt.plot(students,marks)
+# plt.show()
+
+# Question 2
+# Create graph for temperature.
+
+# temp = [32,34,35,38,40]
+# y = [1,2,3,4,5]
+# plt.plot(temp,y)
+# plt.show()
+
+# Question 3
+# Create graph for expenses.
+
+# expense = [5000,6000,7000,8000]
+# income= [10000,20000,30000,25000]
+# plt.plot(expense,income)
+# plt.show()
+
+# -----------------Intermediate Level
+# Question 4
+
+# Question 4 and Question 5
+# months= [2,4,6,8,10]
+# sales_2024= [20000,35000,40000,45000,50000]
+# sales_2025= [32000,45000,56000,52000,49000]
+# plt.plot(months, sales_2024, label="2024")
+# plt.plot(months,sales_2025, label="2025")
+# plt.title("Sales Trend")
+# plt.grid()
+# plt.legend()
+# plt.xlabel("Months")
+# plt.ylabel("Sales-2024-2025")
+# plt.show()
+
+# # -----------------Mini Project
+# Sales Trend Analysis
+
+# month=[1,2,3,4,5]
+# Revenue=[10000,15000,18000,25000,32000]
+# plt.plot(month,Revenue)
+# plt.title("Revenue Trend")
+# plt.grid()
+# plt.legend()
+# plt.xlabel("Months")
+# plt.ylabel("Revenue")
+# plt.show()
+
+
+
